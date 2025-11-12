a9696be7c2948cb7ef99d437fb2a772b87af5272
unknown <harenaaparadisaa@gmail.com>
2025-11-12 21:31:27 +0300

    allowany

--- backend/backend_project/urls.py
+++ backend/backend_project/urls.py
@@ -36,6 +36,8 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('', home),
+
+    # Swagger / Redoc
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
